9da66b7fc0d9517ea1865b8ec25274759970d5c9
Sohamiscurious <118915785+Sohamiscurious@users.noreply.github.com>
2023-08-30 16:15:31 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+
+
+$(document).ready(function(){
+
+    // wow initiation
+    new WOW().init();
+
+    // navigation bar toggle
+    $('#navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(400);
+    });
+
+    // navbar bg change on scroll
+    $(window).scroll(function(){
+        let pos = $(window).scrollTop();
+        if(pos >= 100){
+            $('.navbar').addClass('cng-navbar');
+        } else {
+            $('.navbar').removeClass('cng-navbar');
+        }
+    });
+
+
+
+    // team carousel 
+    $('.team .owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        responsiveClass: true,
+        responsive:{
+            0:{
+                items: 1
+            }, 
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 3
+            }
+        }
+    });
+
+
+
+    // testimonial carousel 
+    $('.testimonial .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav: false,
+        items: 1
+    });
+
+});
